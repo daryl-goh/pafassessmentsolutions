040a452a50131072cffbc7a50b418b1b5b2b8a7d
daryl-goh <goh.daryl@gmail.com>
2023-01-15 18:54:28 +0800

    Task 3c Solution

--- database/schema.sql
+++ database/schema.sql
@@ -20,3 +20,23 @@ values
 ('spongebob', '124 Conch Street, Bikini Bottom', 'spongebob@yahoo.com'),
 ('jessica', '698 Candlewood Land, Cabot Cove', 'fletcher@gmail.com'),
 ('dursley', '4 Privet Drive, Little Whinging, Surrey', 'dursley@gmail.com');
+
+drop table if exists orders;
+
+create table orders (
+    order_id char(8) not null,
+    name varchar(32) not null,
+    order_date date not null,
+    primary key (order_id),
+    foreign key (name) references customers (name)
+);
+
+drop table if exists line_items;
+
+create table line_items (
+    order_id char(8) not null,
+    item varchar(128) not null,
+    quantity int not null,
+    primary key(order_id, item),
+    foreign key (order_id) references orders (order_id)
+)
